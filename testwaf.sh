d0318d7ab84228fc7a8182e5a45282f7091ed2df
Vivek Sinha Anurag <egunda@users.noreply.github.com>
2023-10-13 21:28:46 +0530

    Create testwaf.sh

---
+++ testwaf.sh
@@ -0,0 +1,16 @@
+# Set the JUICESHOP_URL variable if not already done
+# JUICESHOP_URL=<Your Juice Shop URL>
+JUICESHOP_URL=CHANGE ME
+
+# This imitates a Cross Site Scripting attack in POST request
+# This request should be blocked.
+curl -X POST  $JUICESHOP_URL -F "user='<script><alert>Hello></alert></script>'"
+
+# This imitates a SQL Injection attack in POST request
+# This request should be blocked.
+curl -X POST $JUICESHOP_URL -F "user='AND 1=1;"
+
+
+# This imitates a Cross Site Scripting attack in GET request
+# This request should be blocked.
+curl $JUICESHOP_URL -F "user='<script><alert>Hello></alert></script>'"
