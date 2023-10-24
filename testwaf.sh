ae102a223ccb1cd8535316004e6f84e205269006
Vivek Sinha Anurag <egunda@users.noreply.github.com>
2023-10-24 19:03:41 +0530

    Update testwaf.sh
    added more attack simulations

--- testwaf.sh
+++ testwaf.sh
@@ -18,3 +18,20 @@ curl $JUICESHOP_URL -F "user='<script><alert>Hello></alert></script>'"
 # This imitates bad bot user agent "havij"
 # This request should be blocked.
 curl $JUICESHOP_URL -A"havij"
+curl -Ii http://$JUICESHOP_URL -H "User-Agent: blackwidow"
+
+# This imitates LFI attack"
+# This request should be blocked.
+curl -Ii http://$JUICESHOP_URL/ftp/../
+
+# This imitates RCE attack"
+# This request should be blocked.
+curl -Ii http://$JUICESHOP_URL/ftp?doc=/bin/ls
+
+# This imitates HTTP splitting"
+# This request should be blocked.
+curl -Ii "http://$JUICESHOP_URL?foo=advanced%0d%0aContent-Length:%200%0d%0a%0d%0aHTTP/1.1%20200%20OK%0d%0aContent-Type:%20text/html%0d%0aContent-Length:%2035%0d%0a%0d%0a<html>Sorry,%20System%20Down</html>"
+
+# This imitates session fixation"
+# This request should be blocked.
+curl -Ii http://$JUICESHOP_URL -H session_id=X
